44e9073ea4c0529c75a8e8dccecff898d831a098
Miguel da Mota <miguel@damota.de>
2024-10-23 11:16:00 +0200

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,15 @@
-/** Add your relevant code here for the issue to reproduce */
-export default function Home() {
-  return null;
+"use client";
+
+import { anyAction } from "./actions";
+
+export default function Page() {
+	return (
+		<div>
+			<h1>Page</h1>
+
+			<form action={anyAction}>
+				<button>Any action</button>
+			</form>
+		</div>
+	);
 }
